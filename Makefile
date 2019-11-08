5e4a59936f695d756c58fbe8afef27f6bfe250fd
Md Sahil <287mdsahil@gmail.com>
2019-11-09 02:13:04 +0530

    raw mode extended

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=g++
 
 all:
-	$(CC) opentext.cpp -o opentext
+	$(CC) src/opentext.cpp -o opentext
 
 clean:
 	rm -rf opentext
--- src/opentext.cpp
+++ src/opentext.cpp
@@ -23,13 +23,23 @@ void enterRawMode()
 
 	struct termios raw = original_termios;
 	
-	// To disable echo in terminal
-	//raw.c_lflag &= ~(ECHO);
-	// To disable cannonical mode
-	//raw.c_lflag &= ~(ICANON);
-	// To disable both echo and cannonical mode
-	raw.c_lflag &= ~( ECHO | ICANON );
+	// ~ECHO to disable echo
+	// ~ICANON to disable cannonical mode
+	// ~ISIG to disable C^c to quit and C^z to suspend
+	// ~IEXTEN to disable C^v
+	// ~BRKINT to disable break conditions from causing SIGINT signal
+	// ~ISTRIP disables parity checking
+	raw.c_lflag &= ~( BRKINT | ISTRIP | ECHO | ICANON | ISIG );
+	
+	// ~IXON to disable C^s (suspends output) and C^q (resumes output)
+	// ~ICRNL to disable \r translation to \n
+	raw.c_iflag &= ~( IXON | ICRNL );
 
+	raw.c_cflag &= ~( CS8 );
+
+	// ~OPOST to disable processing \n to \n\r
+	raw.c_oflag &= ~( OPOST );
+	
 	// Setting the new attributes
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 
@@ -49,7 +59,7 @@ int main()
 	// Enter 'q' to exit
 	while(read(STDIN_FILENO, &c, 1) == 1 && c !='q')
 	{
-		printf("character entered: %c (%d)\n",c,c);
+		printf("character entered: %c (%d)\n\r",c,c);
 	}	
 	return 0;
 }
