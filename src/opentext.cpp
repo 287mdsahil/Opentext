265ece25304680562a77ed76db468963026b5636
Md Sahil <287mdsahil@gmail.com>
2019-11-10 14:00:44 +0530

    make more portable

--- src/opentext.cpp
+++ src/opentext.cpp
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
+
+//to make more portable
+#ifndef _DEFAULT_SOURCE
+#define _DEFAULT_SOURCE
+#endif
+#ifndef _BSD_SOURCE
+#define _BSD_SOURCE
+#endif
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include "../include/errorhandling"
 #include "../include/globalstate"
 #include "rawmode.h"
